9b375ae8445475c6c9958232e9a2706c353efd57
Travis M. Reynolds <travisreynolds17@gmail.com>
2019-01-09 17:31:47 -0500

    lap commiy

---
+++ dist/js/main.js
@@ -0,0 +1,24 @@
+// const pic1 = "dist/img/officer2.jpeg";
+// const pic2 = "dist/img/outreach.jpg";
+// const pic3 = "dist/img/cop3.jpeg";
+// const pic4 = "dist/img/police-car1.jpeg";
+// const pic5 = "dist/img/crime-scene.jpeg";
+
+// let pics = [pic1, pic2, pic3, pic4, pic5];
+// let count = 0;
+
+// let slide = document.querySelector(".carousel");
+// let timer = window.setInterval(slideshow, 2000);
+
+
+// function slideshow (){
+    
+//     if(count < pics.length){
+//         count++;
+//     } else {
+//         count = 0;
+//     }
+
+//     slide.src = pics[count];
+    
+// }
